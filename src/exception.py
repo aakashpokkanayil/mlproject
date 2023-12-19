e65326b77b4a3c650daedf483b116363d2ab3a1b
aakashpokkanayil <aakashp@Aakashs-Mac-mini.local>
2023-12-20 00:17:05 +0530

    create logging and exception

---
+++ src/exception.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def err_msg_dtls(error,errordetail:sys):
+    _,_,exc_tbl=errordetail.exc_info()
+
+    file_name=exc_tbl.tb_frame.f_code.co_filename
+    line_num=exc_tbl.tb_lineno
+    err=error
+
+    errmsg=f"Error occured in python script name {file_name} line number {line_num} and error message is {err}"
+    return errmsg
+
+
+
+
+class CustomException(Exception):
+    def __init__(self,err_msg,errordetail:sys):
+        super().__init__(err_msg)
+        self.err_msg=err_msg_dtls(err_msg,errordetail)
+
+    def __str__(self):
+        return self.err_msg
+    
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        raise CustomException(e,sys)    
